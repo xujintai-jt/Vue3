4a371a8a8eebe4fe25d81fd666e1d2fe5305525b
xujintai-jt <1791639435@qq.com>
2020-12-28 09:07:26 +0800

    ts学习

---
+++ typescript/04-ts基础类型/js/x.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+ * @Author: xujintai
+ * @Date: 2020-12-26 18:29:18
+ * @LastEditors: xujintai
+ * @LastEditTime: 2020-12-26 18:29:19
+ * @Description: file content
+ * @FilePath: \Vue3\typescript\04-ts基础类型\x.ts
+ */
