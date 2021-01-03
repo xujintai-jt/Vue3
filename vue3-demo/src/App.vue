d56cf0b2634a25bc4350391c6bea4aeedb4f4183
xujintai-jt <1791639435@qq.com>
2021-01-03 19:36:56 +0800

    vue3 hook函数学习

--- vue3-demo/src/App.vue
+++ vue3-demo/src/App.vue
@@ -1,86 +1,28 @@
-<!--
- * @Author: xujintai
- * @Date: 2020-12-31 16:38:52
- * @LastEditors: xujintai
- * @LastEditTime: 2021-01-03 16:59:01
- * @Description: file content
- * @FilePath: \Vue3\vue3-demo\src\App.vue
--->
 <template>
   <div>
-    <h1>父组件</h1>
-    <hr />
-    <child v-if="isShowChild" />
-    <button @click="isShow">显示/隐藏 child组件</button>
+    <h2>页面坐标</h2>
+    <div>
+      <span>X:</span>
+      {{value.x}}
+    </div>
+    <div>
+      <span>Y:</span>
+      {{value.y}}
+    </div>
   </div>
 </template>
 
 <script>
-// 引入方法
-import {
-  ref,
-  onMounted,
-  onUpdated,
-  onUnmounted,
-  onBeforeMount,
-  onBeforeUpdate,
-  onBeforeUnmount,
-} from "vue";
-// 引入子组件
-import Child from "./components/Child";
-
+import useMousePosition from "./hooks/useMousePosition";
 export default {
-  components: { Child },
-  beforeCreate() {
-    console.log("2.x father beforeCreate");
-  },
-  created() {
-    console.log("2.x father created");
-  },
-  beforeMount() {
-    console.log("2.x father beforeMount");
-  },
-  mounted() {
-    console.log("2.x father mounted");
-  },
-  beforeUpdate() {
-    console.log("2.x father beforeUpdate");
-  },
-  updated() {
-    console.log("2.x father updated");
-  },
+  // 需求1: 收集用户鼠标点击的页面坐标
   setup() {
-    console.log("3.0 father setup()");
-    let isShowChild = ref(true);
-    const isShow = function () {
-      isShowChild.value = !isShowChild.value;
-    };
-
-    //生命周期组合API
-    onBeforeMount(() => {
-      console.log("3.0 father onBeforeMount()");
-    });
-
-    onMounted(() => {
-      console.log("3.0 father onMounted()");
-    });
-
-    onBeforeUpdate(() => {
-      console.log("3.0 father onBeforeUpdate()");
-    });
-
-    onUpdated(() => {
-      console.log("3.0 father onUpdated()");
-    });
-
-    return {
-      isShowChild,
-      isShow,
-    };
+    let { value } = useMousePosition();
+    return { value };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
 </style>
 
