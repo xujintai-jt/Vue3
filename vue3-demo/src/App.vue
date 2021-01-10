54703113eb27d12a784c118342524e56cc3c2031
xujintai-jt <1791639435@qq.com>
2021-01-10 19:44:39 +0800

    vue3 学习

--- vue3-demo/src/App.vue
+++ vue3-demo/src/App.vue
@@ -2,39 +2,24 @@
  * @Author: xujintai
  * @Date: 2020-12-31 16:38:52
  * @LastEditors: xujintai
- * @LastEditTime: 2021-01-09 17:25:07
+ * @LastEditTime: 2021-01-10 19:42:58
  * @Description: file content
  * @FilePath: \Vue3\vue3-demo\src\App.vue
 -->
 <template>
-  <div>
-    <h1>app组件</h1>
-    <div>color：{{color}}</div>
-    <button @click="color='red'">red</button>
-    <button @click="color='green'">green</button>
-    <button @click="color='yellow'">yellow</button>
-    <hr />
-    <!-- son组件 -->
-    <Son />
-  </div>
+  <div></div>
 </template>
 
-<script lang="ts">
-//引入方法
-import { ref, provide } from "vue";
-
-//引入子组件
-import Son from "./components/son.vue";
+<script>
+import { reactive, toRefs } from "vue";
 
 export default {
   setup() {
-    const color = ref("red");
-    provide("provide-color", color);
+    const state = reactive({
+      count: 0,
+    });
 
-    return { color };
-  },
-  components: {
-    Son,
+    return {};
   },
 };
 </script>
