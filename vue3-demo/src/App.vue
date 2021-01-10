9eac2b628fb3e33c29c86ea1a08f38cf389c555f
xujintai-jt <1791639435@qq.com>
2021-01-10 19:56:56 +0800

    vue3学习

--- vue3-demo/src/App.vue
+++ vue3-demo/src/App.vue
@@ -2,25 +2,19 @@
  * @Author: xujintai
  * @Date: 2020-12-31 16:38:52
  * @LastEditors: xujintai
- * @LastEditTime: 2021-01-10 19:42:58
+ * @LastEditTime: 2021-01-10 19:51:57
  * @Description: file content
  * @FilePath: \Vue3\vue3-demo\src\App.vue
 -->
 <template>
-  <div></div>
+  <h1>App组件</h1>
+  <son />
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
-
+import Son from "./components/son";
 export default {
-  setup() {
-    const state = reactive({
-      count: 0,
-    });
-
-    return {};
-  },
+  components: { Son },
 };
 </script>
 
---
+++ vue3-demo/src/components/son.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: xujintai
+ * @Date: 2021-01-10 19:47:29
+ * @LastEditors: xujintai
+ * @LastEditTime: 2021-01-10 19:55:01
+ * @Description: file content
+ * @FilePath: \Vue3\vue3-demo\src\components\son.vue
+-->
+<template>
+  <h1>Son组件</h1>
+  <button @click="isShow=true">显示对话框</button>
+  <!-- 对话框 -->
+  <teleport to="body">
+    <div v-if="isShow">
+      <h1>Hello World!</h1>
+      <button @click="isShow=false">隐藏对话框</button>
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const isShow = ref(false);
+
+    return {
+      isShow,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
